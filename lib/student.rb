bde8ad2c554fc0df926459f329f58d2c583a88b0
Jacqueline Lee <anginblu@gmail.com>
2017-12-27 18:17:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,15 +3,9 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-  @@all = []
   attr_accessor :id, :name, :grade
 
-  def initialize(id: nil, name: nil, grade: nil)
-    @id = id
-    @name = name
-    @grade = grade
-    @@all << self
-  end
+
 
 
 end
